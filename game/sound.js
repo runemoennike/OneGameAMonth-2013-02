6d75fb47a69a7115943a06916999a1517c07e3d8
rm <rm@kirkeweb.dk>
2013-02-10 15:25:23 +0100

    Per-clip sound volume leveling

--- game/sound.js
+++ game/sound.js
@@ -4,11 +4,17 @@ var sfxManifest = [
     { src: sfxPath + "pulse.mp3|" + sfxPath + "pulse.ogg", id: "bgmusic_1", data: 1 },
     { src: sfxPath + "countdown.mp3|" + sfxPath + "countdown.ogg", id: "countdown", data: 1 },
     { src: sfxPath + "deathnoise.mp3|" + sfxPath + "deathnoise.ogg", id: "deathnoise", data: 1 },
+    { src: sfxPath + "pewpewpew.mp3|" + sfxPath + "pewpewpew.ogg", id: "pewpewpew", data: 1 },
 ];
+
+var sndLevels = {
+    "pewpewpew": 0.2
+}
 
 function playSound(id) {
     //Play the sound: play (src, interrupt, delay, offset, loop, volume, pan)
-    var instance = createjs.SoundJS.play(id, createjs.SoundJS.INTERRUPT_ANY, 0, 0, false, 1);
+    var volume = (typeof sndLevels[id] !== "undefined") ? sndLevels[id] : 1;
+    var instance = createjs.SoundJS.play(id, createjs.SoundJS.INTERRUPT_ANY, 0, 0, false, volume);
     if (instance == null || instance.playState == createjs.SoundJS.PLAY_FAILED) { return; }
 }
 
